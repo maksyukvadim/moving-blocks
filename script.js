b9f5895f43740bcc75aa9e7f67165861e64bd9e3
Vadym Maksyuk <maksyukvadim@gmail.com>
2016-06-27 16:09:59 +0300

    Fixed bugs placeholder

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
-
 $(function () {
     var rang = JSON.parse(localStorage.getItem('rang')) || [],
         div = $("#blockContainer div"),
         arr = [];
-
     for (var i = 0; i < rang.length; i++) {
         arr.push(div[rang[i] - 1])
     }
     rang.length && $(arr)
         .appendTo($("#blockContainer")) && $(".out")
-        .text(rang);;
+        .text(rang);
+    var blockClass = " ui-corner-all portlet-placeholder";
+    function clas() {
+        return function () {
+            $('#blockContainer div').mousedown(function () {
+                console.log($(this).attr('id'));
+                if($(this).attr('id') == "block10") {
+                    blockClass = " ui-corner-all portlet-placeholderBig";
+                }else {
+                    blockClass = " ui-corner-all portlet-placeholder";
+                }
+            });
+            return blockClass;
+        }
+    }
+
+    var b = clas();
+    var c = clas();
     $("#blockContainer")
         .sortable(
 
-            {
-               
-                placeholder: " ui-corner-all portlet-placeholder",
-                dropOnEmpty: false,
-            stop: function (event, ui) {
-                rang = [];
-                $("#blockContainer div")
-                    .each(function (indx, element) {
-                        rang.push(1 + div.index(element) + " ");
-                    });
-                $(".out")
-                    .text(rang);
-                localStorage.setItem('rang', JSON.stringify(rang));
+    {
+        placeholder: c(),
+        start: function (event, ui) {
+            ui.item.css("height","200px");
+            if(ui.item.attr('id')=="block10"){
+                ui.placeholder.addClass("col-md-3  portlet-placeholderBig");
+            } else {
+                ui.placeholder.addClass("col-md-3 square portlet-placeholder");
             }
-        });
+            $('div .ui-corner-all').removeClass('ui-corner-all ')
+        },
+
+                dropOnEmpty: false,
+                stop: function (event, ui) {
+                    rang = [];
+                    $("#blockContainer div")
+                        .each(function (indx, element) {
+                            rang.push(1 + div.index(element) + " ");
+                        });
+                    $(".out")
+                        .text(rang);
+                    localStorage.setItem('rang', JSON.stringify(rang));
+                }
+            });
     $('#blockContainer').sortable('disable');
     $("#blockContainer").disableSelection();
 });
@@ -35,20 +59,20 @@ $(function () {
 
 $(function () {
     $("#btn1").click(function () {
-        var rang = ["1 ","2 ","3 ","4 ","5 ","6 ","7 ","8 ","9 ","10 "],
+        var rang = ["1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 ", "8 ", "9 ", "10 "],
             div = $("#blockContainer div"),
             arr = [];
         localStorage.setItem('rang', JSON.stringify(rang));
         var containerBlock = $("#blockContainer");
-        for(var i = 0; i < containerBlock.children().length; i++ ){
-            containerBlock.children().eq[i] = $("#block"+ (i+1)).css({'position':'static'});
+        for (var i = 0; i < containerBlock.children().length; i++) {
+            containerBlock.children().eq[i] = $("#block" + (i + 1)).css({'position': 'static'});
         }
 
         $('#blockContainer:only-child').remove();
-        for(var i = 0; i < containerBlock.children().length; i++ ){
+        for (var i = 0; i < containerBlock.children().length; i++) {
             $('#blockContainer').append(containerBlock.children().eq[i]);
         }
-        $( "#blockContainer" ).sortable();
+        $("#blockContainer").sortable();
     });
     $("#btn2").click(function () {
         var i = 1;
@@ -61,14 +85,11 @@ $(function () {
         }
 
         function test2() {
-            $('#block' +i ).addClass("shake shake-constant");
+            $('#block' + i).addClass("shake shake-constant");
             i++;
         }
 
         test();
-
-
-
         $('#blockContainer').sortable('enable');
         $("#btn2").hide();
         $("#btn3").show();
@@ -79,5 +100,5 @@ $(function () {
         $("#btn2").show();
         $("#btn3").hide();
     });
-    });
+});
 
